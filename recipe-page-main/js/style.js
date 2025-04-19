30aba0e40f65c008aa5fe0c99bf4caec2963f777
shaoqiu Pan <stephenpsq@gmail.com>
2025-04-19 17:13:19 +0800

    done

--- recipe-page-main/js/style.js
+++ recipe-page-main/js/style.js
@@ -1,6 +1,20 @@
-table = {
+table_datas = {
   Calories: "277kcal",
   Carbs: "0g",
   Protein: "20g",
   Fat: "22g",
 };
+
+const table = document.querySelector(".recipe-card__nutrition-table");
+
+let categoris = Object.keys(table_datas);
+var tr, td1, td2;
+categoris.forEach((category) => {
+  tr = document.createElement("tr");
+  td1 = document.createElement("td");
+  td2 = document.createElement("td");
+  td1.textContent = category;
+  td2.textContent = table_datas[category];
+  tr.append(td1, td2);
+  table.appendChild(tr);
+});
